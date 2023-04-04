d409bbe7fe1d24e14cb66a79792fac9ff182077b
abuzar08 <ak.abuzar@gmail.com>
2023-04-04 12:54:24 -0400

    small edit

--- localize.sh
+++ localize.sh
@@ -4,6 +4,7 @@ git clone https://github.com/cadia-lvl/qa-crowdsourcing-api.git
 cd qa-crowdsourcing-app && rm -r -f .git && cd ..
 cd qa-crowdsourcing-api && rm -r -f .git && cd ..
 
+# TODO:
 # pre-localization check.
 # It asks, given the warning, do you want to continue? [y/n]
 #   y: If yes, then continue with replacing with warnings.
