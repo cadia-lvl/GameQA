ee17e44a2829c0dda06fd30df0cb655896bf28aa
abuzar08 <ak.abuzar@gmail.com>
2023-04-03 23:10:50 -0400

    text-done, emojis-done, values-remaining

--- localization_text.py
+++ localization_text.py
@@ -12,22 +12,30 @@ def build_repl_dict(df: pd.DataFrame, args):
     src = df[args.src_col]
     repls = df[args.tgt_col]
     
-    repl_dict = {}
-    for key,val in zip(src, repls):
-        repl_dict[key] = val
+    if args.verbose:
+        print("building repl dictionary...")
     
-    print("build dict")
+    repl_dict = {key: val for key, val in zip(src, repls)}
+    
+    if args.verbose:
+        print("Built repl dictionary.")
+        
     return repl_dict
 
-def load_sheet(path):
+def load_sheet(path, args):
     '''
     Read sheet as csv and return.
     '''
-    df = pd.read_csv(path)
-    df = df[df.repo == 'qa-crowdsourcing-api']
-    df = df[df["translation_needed"]!='not sure']
     
-    print('loaded sheet')
+    if args.verbose:
+        print('loading repl sheet...')
+        
+    df = pd.read_csv(path)
+    df = df[df["translation_needed"]!='not sure'] # TODO: Remove this when translations are clean.
+    
+    if args.verbose:
+        print('loaded repl sheet')
+    
     return df
 
 def replace_text_in_string(line, repl_dict):
@@ -43,13 +51,21 @@ def replace_text_in_string(line, repl_dict):
         for start,end in occurances:
             left = line[:start]
             right = line[end:]
-            assert type(left) == str and type(right) == str and type(value) == str, f"types are weird. Left - {type(left)} {left};  Right - {type(right)} {right};  value - {type(value)} {value}"
+            assert type(left) == str and type(right) == str and type(value) == str, \
+                f"types are weird. Left - {type(left)} {left};  Right - {type(right)} {right};  value - {type(value)} {value}"
             line = left + value + right
     
     return line
     # raise NotImplementedError
 
-def replace_text_in_file(source_file, target_file, repl_dict):
+def replace_text_in_file(source_file, target_file, repl_dict, args):
+    '''
+    Performs replacements in source_file, and writes into target_file.
+    Source and target are the same by default, but can be specified to be different.
+    '''
+    
+    if args.very_verbose:
+        print(f"Replacing text in file: {source_file}...")
     
     with open(source_file, 'r') as f:
         lines = f.readlines()
@@ -60,13 +76,16 @@ def replace_text_in_file(source_file, target_file, repl_dict):
         new_line = replace_text_in_string(line, repl_dict)
         new_lines.append(new_line)
     
+    if args.very_verbose:
+        print(f"Replaced. Writing new text in file: {target_file}")
+    
     with open(target_file, 'w') as f:
         f.writelines(new_lines)
 
-def replace_text_all(files, repl_dict):
+def replace_text_all(files, repl_dict, args):
     
     for file_path in files:
-        replace_text_in_file(file_path, file_path, repl_dict)
+        replace_text_in_file(file_path, file_path, repl_dict, args)
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -84,18 +103,26 @@ def get_args():
                         help='''This is the directory that the localizer with walk through to make
                         The translations.''')
     
+    parser.add_argument('--verbose', '-v', type=bool, default=False,
+                        help="Set verbose to print checkopints.")
+    
+    parser.add_argument('--very_verbose', '-vv', type=bool, default=False,
+                        help="Set very verbose to print more checkpoints.")
+    
     args = parser.parse_args()
     print(args)
+    
+    if args.very_verbose:
+        args.verbose = True
     
     return args
 
 if __name__ == "__main__":
     
     args = get_args()
+    print(f"Replacing Text from all *.ts[x] files in the directory {args.dir} using {args.repl_file}.")
+    
     df = load_sheet(args.repl_file)
     repl_dict = build_repl_dict(df, args)
     files = list(Path(args.dir).rglob("*.ts[x]"))
-    replace_text_all(files, repl_dict)
-    
-    
-        
+    replace_text_all(files, repl_dict, args)
